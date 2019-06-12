554a9186e3f3c3cf7bf941a019ca3a81c73b9d00
boneyqb <boney@qburst.com>
2019-06-12 10:18:48 +0530

    to test lambda pipeline

---
+++ src/test/java/com/lambda/lambdaTest/LambdaTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lambda.lambdaTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LambdaTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
